d430eb3c4317c0b3232e25b01e107d86ad07b218
Tấn Thành <129248195+IQRYTI@users.noreply.github.com>
2024-06-19 18:35:56 +0700

    NHẠC SĨ TẤN THÀNH

---
+++ tan_thanh.py
@@ -0,0 +1,11 @@
+# Thông tin về nhạc sĩ Tấn Thành
+name = "Tấn Thành"
+date_of_birth = "24/02/2004"
+hometown = "Hoài Ân, Bình Định"
+current_city = "TP Hồ Chí Minh"
+
+# In ra các thông tin
+print(f"Thông tin về nhạc sĩ {name}:")
+print(f"- Ngày sinh: {date_of_birth}")
+print(f"- Quê quán: {hometown}")
+print(f"- Đang sinh sống và làm việc tại: {current_city}")
